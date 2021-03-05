1b2595cf2b1f18684adbc76589353315828caf9c
petrusCode <pedro_hot32@hotmail.com>
2021-03-05 06:32:26 +0100

    Testing the dependencies from GsonCrud

---
+++ src/main/java/com/prueba/dependencies/probando/Prueba.java
@@ -0,0 +1,14 @@
+package com.prueba.dependencies.probando;
+
+import java.io.IOException;
+
+import com.netmind.presentation.Menu;
+
+public class Prueba {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Menu.studentMenu();
+	}
+
+}
